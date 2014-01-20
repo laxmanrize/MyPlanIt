9e9128ce4485e6e274b187d79167e6a01d677689
laxman <arunkumar@Aruns-iMac.local>
2014-01-20 15:33:20 +0530

    Initial Commit

---
+++ MyPlanIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyPlanIt
+//
+//  Created by Arun Kumar on 20/01/14.
+//  Copyright (c) 2014 SSTIG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
